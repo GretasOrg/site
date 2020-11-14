359847ca7b3d36ec15e02ed8d65a85803f0a760b
Silas Pedrosa <silas@implystudio.com>
2020-11-14 17:50:05 -0300

    [ADD] plan with free input

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,13 +5,34 @@
     const link = meal.querySelector(".meals__item-button a");
 
     const plan = meal.getAttribute("data-plan");
-    const price = meal.getAttribute("data-price");
 
     dropdown &&
       dropdown.addEventListener("dropdown::select", (ev) => {
         const frequency = ev.detail.value;
+        meal.setAttribute("data-frequency", frequency);
+        const price = meal.getAttribute("data-price");
         const newLink = `https://gretas.faturasimples.com.br/contratar/gretas?plano=${plan}&valor=${price}.00&recorrencia=${frequency}`;
         link.href = newLink;
       });
+  });
+})();
+
+(async function () {
+  document.addEventListener("DOMContentLoaded", (ev) => {
+    const planInputEl = document.querySelector(
+      ".meals__item-price .price.input"
+    );
+    const mealEl = document.querySelector("#mealCustom");
+    const link = mealEl.querySelector(".meals__item-button a");
+    const plan = mealEl.getAttribute("data-plan");
+
+    planInputEl.addEventListener("change", (ev) => {
+      const frequency = mealEl.getAttribute("data-frequency");
+      const price = ev.target.value;
+      mealEl.setAttribute("data-price", price);
+
+      const newLink = `https://gretas.faturasimples.com.br/contratar/gretas?plano=${plan}&valor=${price}.00&recorrencia=${frequency}`;
+      link.href = newLink;
+    });
   });
 })();
