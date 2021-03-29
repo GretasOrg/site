904b007b576046a871609a377851dd817b8db2a1
Silas Pedrosa <silas@capyba.com>
2021-03-29 17:03:55 -0300

    [ADD] site metadata no gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,38 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Gretas - Fortalecendo o Sertão`,
+    description: `Trabalhamos com as comunidades rurais de Ibimirim
+    no estímulo à autonomia e empoderamento de todos
+    que nelas fazem morada, através de programas
+    focados em saúde, segurança alimentar, educação e
+    fonte de renda. Contribuímos para a diminuição das
+    desigualdades sociais e promoção da dignidade
+    humana.`,
+
+    // <link rel="canonical" href="https://www.gretas.org/" />
+    // <meta name="robots" content="index, follow" />
+
+    // <meta property="og:type" content="article" />
+    // <meta property="og:title" content="Gretas - Fortalecendo o Sertão" />
+    // <meta
+    //   property="og:description"
+    //   content="Trabalhamos com as comunidades rurais de Ibimirim no estímulo à autonomia e empoderamento de todos que nelas fazem morada, através de programas focados em saúde, segurança alimentar, educação e fonte de renda. Contribuímos para a diminuição das desigualdades sociais e promoção da dignidade humana."
+    // />
+    // <meta property="og:image" content="https://gretas.org/assets/images/mulher-simbolo.jpg" />
+    // <meta property="og:url" content="https://www.gretas.org/" />
+    // <meta property="og:site_name" content="Projeto Gretas" />
+
+    // <meta name="twitter:title" content="Gretas - Fortalecendo o Sertão" />
+    // <meta
+    //   name="twitter:description"
+    //   content="Trabalhamos com as comunidades rurais de Ibimirim no estímulo à autonomia e empoderamento de todos que nelas fazem morada, através de programas focados em saúde, segurança alimentar, educação e fonte de renda. Contribuímos para a diminuição das desigualdades sociais e promoção da dignidade humana."
+    // />
+    // <meta name="twitter:image" content="https://gretas.org/assets/images/mulher-simbolo.jpg" />
+    // <meta name="twitter:site" content="Projeto Gretas" />
+    // <meta name="twitter:creator" content="Projeto Gretas" />
+  },
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
