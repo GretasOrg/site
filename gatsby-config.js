b04c1454c22707cf9cea0e1f5692e102197b8105
Silas Pedrosa <silas@capyba.com>
2021-03-30 15:05:47 -0300

    [ADD] remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,28 +15,6 @@ module.exports = {
     fonte de renda. Contribuímos para a diminuição das
     desigualdades sociais e promoção da dignidade
     humana.`,
-
-    // <link rel="canonical" href="https://www.gretas.org/" />
-    // <meta name="robots" content="index, follow" />
-
-    // <meta property="og:type" content="article" />
-    // <meta property="og:title" content="Gretas - Fortalecendo o Sertão" />
-    // <meta
-    //   property="og:description"
-    //   content="Trabalhamos com as comunidades rurais de Ibimirim no estímulo à autonomia e empoderamento de todos que nelas fazem morada, através de programas focados em saúde, segurança alimentar, educação e fonte de renda. Contribuímos para a diminuição das desigualdades sociais e promoção da dignidade humana."
-    // />
-    // <meta property="og:image" content="https://gretas.org/assets/images/mulher-simbolo.jpg" />
-    // <meta property="og:url" content="https://www.gretas.org/" />
-    // <meta property="og:site_name" content="Projeto Gretas" />
-
-    // <meta name="twitter:title" content="Gretas - Fortalecendo o Sertão" />
-    // <meta
-    //   name="twitter:description"
-    //   content="Trabalhamos com as comunidades rurais de Ibimirim no estímulo à autonomia e empoderamento de todos que nelas fazem morada, através de programas focados em saúde, segurança alimentar, educação e fonte de renda. Contribuímos para a diminuição das desigualdades sociais e promoção da dignidade humana."
-    // />
-    // <meta name="twitter:image" content="https://gretas.org/assets/images/mulher-simbolo.jpg" />
-    // <meta name="twitter:site" content="Projeto Gretas" />
-    // <meta name="twitter:creator" content="Projeto Gretas" />
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -56,6 +34,28 @@ module.exports = {
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     },
   ],
